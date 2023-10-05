e383e738bd106a8057ad0f945a504278bf1c8049
Carsten Jantzen <carsten.jantzen@invixo.com>
2023-10-05 19:38:21 +0000

    Assignment without using GitHub Copilot Done

--- Assignment02/ass2.py
+++ Assignment02/ass2.py
@@ -1,13 +1,47 @@
-import re
+"""This runs assignment 2"""
+import os
+
+
+def find_occurrences(string, substring):
+    """Finds occurrences in string by the pattern
+
+    Keyword arguments:
+    strin -- the string to look in
+    substring -- the pattern to look for
+
+    """
+    # Initialize count and start to 0
+    start = 0
+    positions = []
+
+    # Search through the string till
+    # we reach the end of it
+    while start < len(string):
+
+        # Check if a substring is present from
+        # 'start' position till the end
+        pos = string.find(substring, start)
+        if pos != -1:
+            # If a substring is present, move 'start' to
+            # the next position from start of the substring
+            positions.append(str(pos))
+            start = pos + 1
+        else:
+            # If no further substring is present
+            break
+    # return the positions
+    return positions
+
 
 LINES = None
-RESULT_FILE = open("result_ass2.txt", "a")
-with open('stringmultimatching.in') as f:
+if os.path.isfile("result_ass2.txt"):
+    os.remove("result_ass2.txt")
+RESULT_FILE = open("result_ass2.txt", "a", encoding='utf-8')
+with open('stringmultimatching.in', encoding='utf-8') as f:
     LINES = [ line.strip() for line in f ]
 
 PATTERN_LINES = 0
-pattern_list= []
-RESULT = None
+pattern_list = []
 for line in LINES:
     # Start new pattern
     if line.isdigit():
@@ -18,14 +52,11 @@ for line in LINES:
         if len(line) > 100000:
             raise TypeError("pattern to long")
         pattern_list.append(line)
-
     else:
+        # The total length of line is no more than 200.000.
         if len(line) > 200000:
             raise TypeError("input line to long")
-        for pattern in pattern_list:
-            iter = re.finditer(pattern, line)
-            for m in iter:
-                RESULT_FILE.write(str(m.start()))
-                RESULT_FILE.write(" ")
-            RESULT_FILE.write("\n")
+        for index, pattern in enumerate(pattern_list):
+            OCC = find_occurrences(line, pattern)
+            RESULT_FILE.write(" ".join(OCC) + "\n")
 RESULT_FILE.close()
--- Assignment02/test_ass2.py
+++ Assignment02/test_ass2.py
@@ -4,4 +4,4 @@ import unittest
 
 class TestAss2(unittest.TestCase):
     def test_ass(self):
-        self.assertTrue(filecmp.cmp("./stringmultimatching.ans", "./result_test.txt", shallow=False))
+        self.assertTrue(filecmp.cmp("./stringmultimatching.ans", "./result_ass2.txt", shallow=False))

adcc77a12a37229fc7bb9ce937a2a65d9e70864e
Carsten Jantzen <carsten.jantzen@invixo.com>
2023-10-05 22:12:32 +0200

    Update test_ass1.py

--- Assignment01/test_ass1.py
+++ Assignment01/test_ass1.py
@@ -1,1 +1,7 @@
+import filecmp
+import pytest
+import unittest
 
+class TestAss2(unittest.TestCase):
+    def test_ass(self):
+        self.assertTrue(filecmp.cmp("./airlinehub.ans", "./result_ass1.txt", shallow=False))

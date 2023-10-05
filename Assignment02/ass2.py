834f6dd82857e21d4c806f69cd81f19cd23e0a46
Carsten Jantzen <carsten.jantzen@invixo.com>
2023-10-05 13:43:07 +0000

    ready for compare

--- Assignment02/ass2.py
+++ Assignment02/ass2.py
@@ -1,16 +1,27 @@
-lines = None
-with open('stringmultimatching.in') as f:
-    lines = [ line.strip() for line in f ]
+import re
 
-pattern_lines = 0
-patterns_read = 0
+LINES = None
+RESULT_FILE = open("result_ass2.txt", "a")
+with open('stringmultimatching.in') as f:
+    LINES = [ line.strip() for line in f ]
+
+PATTERN_LINES = 0
 pattern_list= []
-for line in lines:
+RESULT = None
+for line in LINES:
     # Start new pattern
     if line.isdigit():
-        pattern_lines = int(line)
-        patterns_read = 0
+        PATTERN_LINES = int(line)
         pattern_list= []
-    elif len(pattern_list) < pattern_lines:
+    elif len(pattern_list) < PATTERN_LINES:
         pattern_list.append(line)
-    print(pattern_list)
+    else:
+        for pattern in pattern_list:
+            iter = re.finditer(pattern, line)
+            for m in iter:
+                RESULT_FILE.write(str(m.start()))
+                RESULT_FILE.write(" ")
+            RESULT_FILE.write("\n")
+            
+            
+RESULT_FILE.close()

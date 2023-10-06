93390222abcffcc115d7a4551cfa6cd4add12f34
Carsten Jantzen <carsten.jantzen@invixo.com>
2023-10-06 10:16:10 +0000

    add draft

--- Assignment01/airhub_finder2.py
+++ Assignment01/airhub_finder2.py
@@ -1,8 +1,17 @@
 import os
-from math import radians, sin, cos, sqrt, atan2
+from geopy.distance import geodesic
+
 
 def read_coords_from_file(filename):
-    
+    """
+    Read coordinates from file and return a list of coordinates.
+
+    Args:
+        filename (str): The name of the file to read coordinates from.
+
+    Returns:
+        list: A list of coordinates.
+    """
     with open(filename, 'r') as f:
         coords = []
         while True:
@@ -19,31 +28,28 @@ def read_coords_from_file(filename):
                     coord_list.append([lat, lon])
             coords.append(coord_list)
         return coords
-    
-# BEGIN: 2j3b4k5l6m7n
-
-def calculate_distance(coord1, coord2):
-    R = 6371  # radius of the earth in km
-    lat1, lon1 = coord1
-    lat2, lon2 = coord2
-    dlat = radians(lat2 - lat1)
-    dlon = radians(lon2 - lon1)
-    a = sin(dlat / 2) * sin(dlat / 2) + cos(radians(lat1)) * cos(radians(lat2)) * sin(dlon / 2) * sin(dlon / 2)
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    distance = R * c
-    return distance
 
 
-def calculate_total_distance(coords):
-    total_distance = 0
-    for i, coord1 in enumerate(coords):
-        for j, coord2 in enumerate(coords):
-            if i < j:
-                distance = calculate_distance(coord1, coord2)
-                total_distance += distance
-    return total_distance
+def find_airhub(coords):
+    """
+    Find first airhub with the highest distance in list of coordinates.
 
+    Args:
+        coords (list): A list of coordinates.
 
+    Returns:
+        list: The coordinates of the airhub with the highest distance.
+    """
+    max_distance = 0
+    airhub = None
+    for coord in coords:
+        for other_coord in coords:
+            if coord != other_coord:
+                r1 = geodesic(coord, other_coord).km
+                if r1 > max_distance:
+                    max_distance = r1
+                    airhub = coord
+    return airhub
 
 
 if __name__ == '__main__':
@@ -51,11 +57,9 @@ if __name__ == '__main__':
         os.remove('result_ass1.txt')
     coords = read_coords_from_file('airlinehub.in')
     for coord_set in coords:
-        for coord in coord_set:
-            print(coord)
-            print(calculate_total_distance(coord_set))
-        # for i, coord1 in enumerate(coord_set):
-        #     for j, coord2 in enumerate(coord_set):
-        #         if i < j:
-        #             distance = calculate_distance(coord1, coord2)
-        #             print(f"Distance between {coord1} and {coord2}: {distance:.2f} km")
+        #write airhub result to file and format float with 2 decimal places
+        result = find_airhub(coord_set)
+        with open("result_ass1.txt", "a") as f:
+            f.write("{:.2f}, {:.2f}\n".format(result[0], result[1]))
+        # print result list with 2 decimal places
+        print("{:.2f}, {:.2f}".format(result[0], result[1]))
